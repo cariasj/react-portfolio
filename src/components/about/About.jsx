cf5d277393d8b8a7cd768ef7422678fe25a6aedd
charon <cariasjosue@gmail.com>
2022-05-26 11:30:19 -0400

    portfolio cont

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,8 +3,10 @@ import './about.css'
 const About = () => {
   return (
     <section id='about'>
-      My name is Josue and I am a full stack developer specializing in javascript and related frameworks. I attended University
-			of Miami coding bootamp for 6 months and received my certificate of completion upon successful graduation meeting all requirments. 
+      <h2>About</h2>
+        <p> My name is Josue and I am a full stack developer specializing in javascript and related frameworks. I attended University
+			      of Miami coding bootamp for 6 months and received my certificate of completion upon successful graduation meeting all requirments.
+        </p>
     </section>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import './portfolio.css'
 import project1 from '../../assets/project1.png'
+import project2 from '../../assets/project2.png'
+import project3 from '../../assets/project3.png'
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
+      <h2> Recent Works </h2>
+      <div className='conatiner portfolio_container'>
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+            <img src= {project1} alt=''></img>
+            <h3> Title of projects</h3>
+            <a href='https://github.com/cariasj' className='btn'> Github</a>
+            <a href='https://github.com/cariasj' className='btn btn-primary'> Github</a>
+          </div>
+        </article>
 
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+            <img src= {project2} alt=''></img>
+            <h3> Title of projects</h3>
+            <a href='https://github.com/cariasj' className='btn'> Github</a>
+            <a href='https://github.com/cariasj' className='btn btn-primary'> Github</a>
+          </div>
+        </article>
 
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+            <img src= {project3} alt=''></img>
+            <h3> Title of projects</h3>
+            <a href='https://github.com/cariasj' className='btn'> Github</a>
+            <a href='https://github.com/cariasj' className='btn btn-primary'> Github</a>
+          </div>
+        </article>
+
+      </div>
 		</section>
 					
 		
