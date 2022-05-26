a05f0e32bbeed4b4e233a82df565587decdd082e
charon <cariasjosue@gmail.com>
2022-05-26 11:39:52 -0400

    portfolio complete

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,27 +12,27 @@ const Portfolio = () => {
         <article className='portfolio_item'>
           <div className='portfolio_item-image'>
             <img src= {project1} alt=''></img>
-            <h3> Title of projects</h3>
-            <a href='https://github.com/cariasj' className='btn'> Github</a>
-            <a href='https://github.com/cariasj' className='btn btn-primary'> Github</a>
+            <h3> Project 1</h3>
+            <a href='https://cariasj.github.io/project-1-team-8/' className='btn'> Link</a>
+           
           </div>
         </article>
 
         <article className='portfolio_item'>
           <div className='portfolio_item-image'>
             <img src= {project2} alt=''></img>
-            <h3> Title of projects</h3>
-            <a href='https://github.com/cariasj' className='btn'> Github</a>
-            <a href='https://github.com/cariasj' className='btn btn-primary'> Github</a>
+            <h3> Horiseon </h3>
+            <a href='https://cariasj.github.io/Challenge1-Refactor/' className='btn'> Link</a>
+            
           </div>
         </article>
 
         <article className='portfolio_item'>
           <div className='portfolio_item-image'>
             <img src= {project3} alt=''></img>
-            <h3> Title of projects</h3>
-            <a href='https://github.com/cariasj' className='btn'> Github</a>
-            <a href='https://github.com/cariasj' className='btn btn-primary'> Github</a>
+            <h3>  Project 2</h3>
+            <a href='https://blah-blah-blog1.herokuapp.com/'className='btn'> Link</a>
+        
           </div>
         </article>
 
